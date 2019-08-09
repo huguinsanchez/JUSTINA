4fd11463d22e98ff9a519ed281c6e5cc0ecf679d
huguinsanchez <v-hugo-s@hotmail.com>
2019-08-08 19:36:15 -0500

    mvncdf services change to cpp working

--- catkin_ws/src/navigation/localization/scripts/prob_loc_amcl.py
+++ catkin_ws/src/navigation/localization/scripts/prob_loc_amcl.py
@@ -1,31 +1,50 @@
 #! /usr/bin/env python
 import sys
-#from localization.srv import *
+from localization.srv import *
 from navig_msgs.srv import prob_localization
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped as pwcs
-from std_msgs.msg import Float32
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Float64
 from oct2py import octave
 import numpy as np
 import time
 import math
+
+from scipy.stats import multivariate_normal
 
 octave.addpath('/usr/share/octave/packages/statistics-1.2.4/')
 global cov
 global mean
 global limits_dict_min
 global limits_dict_max
+global cov_pc
+global client_mvncdf
 
 def truncate(number, digits):
-    stepper = 10.0 ** digits
-    return math.trunc(stepper * number) / stepper
+	stepper = 10.0 ** digits
+	return math.trunc(stepper * number) / stepper
+#def mvn_cdf(xl,xu,mean,cov,step):
+#	aux_x=0.0
+#	sum=0.0
+#	step_sq=step*step
+#	print len(np.arange(xl[0],xu[0],step))
+#	for i in np.arange(xl[0],xu[0],step):
+#		#aux_x=multivariate_normal.pdf(,mean=mean, cov=cov)
+#		for j in np.arange(xl[1],xu[1],step):
+#			#print "entre" + str(i)
+#			sum+=step_sq*multivariate_normal.pdf([i,j],mean=mean, cov=cov)
+#			#print sum
+#	return sum
 
 def  handle_prob_loc(req):
 	global cov
 	global mean
 	global limits_dict_min
 	global limits_dict_max
-	result=Float32()
+	global cov_pc
+	global client_mvncdf
+	result=Float64()
 	cov[0][0]=truncate(cov[0][0],6)
 	cov[0][1]=truncate(cov[0][1],6)
 	cov[1][0]=truncate(cov[1][0],6)
@@ -33,14 +52,22 @@ def  handle_prob_loc(req):
 	#for i in cov:
 	#	if i<0:
 	#		i=0
+	print "limit inf"+ str(limits_dict_min[req.room])
+	print "limit max"+ str(limits_dict_max[req.room])
 	print"mean(pose)"
 	print mean
-	print "cov"
-	print cov
-	eigen_vals=np.linalg.eig(cov)
-	print"eigen values"
-	print eigen_vals
-	result.data=octave.mvncdf(limits_dict_min[req.room],limits_dict_max[req.room],mean,cov)
+	#print "cov"
+	#print cov
+	#eigen_vals=np.linalg.eig(cov)
+	#print"eigen values"
+	#print eigen_vals
+	#result.data=octave.mvncdf(limits_dict_min[req.room],limits_dict_max[req.room],mean,cov)
+	print"cov pc"
+	print cov_pc
+	#print octave.mvncdf(limits_dict_min[req.room],limits_dict_max[req.room],mean,cov)
+	aux=client_mvncdf(limits_dict_min[req.room][0],limits_dict_min[req.room][1],limits_dict_max[req.room][0],limits_dict_max[req.room][1],mean[0],mean[1],0.001,cov_pc[0][0],cov_pc[0][1],cov_pc[1][0],cov_pc[1][1])
+	result.data=aux.prob
+	print result
 	return result
 
 def limits(file):
@@ -81,28 +108,46 @@ def callback_pose(msg):
 	mean=np.array([msg.pose.pose.position.x,msg.pose.pose.position.y])
 	#print mean
 
+def callback_particlecloud(msg):
+	global cov_pc
+	#print"LOCALIZATION NODE PARTICLE CLOUD"
+	#print msg.poses[0].position
+	particlecloud=list()
+	for i in range(len(msg.poses)):
+		particlecloud.append([msg.poses[i].position.x,msg.poses[i].position.y])
+	particlecloud=np.array(particlecloud)
+	cov_pc=np.cov(particlecloud,rowvar=False)
+	#print"particlecloud"
+	#print particlecloud
+	#print "cov_pc"
+	#print cov_pc
+
 def main(file):
 	global limits_dict_min
 	global limits_dict_max
+	global client_mvncdf
+
 	limits_dict_min=dict()
 	limits_dict_max=dict()
 	print"Initialiing Probability of position"
 	print"File of locations: "+str(file)
 	rospy.init_node('prob_loc_amcl', anonymous=True)
 	rospy.Subscriber("/navigation/localization/amcl_pose", pwcs, callback_pose)
+	rospy.Subscriber("/navigation/localization/particlecloud", PoseArray, callback_particlecloud)
 	limits(file)
 	rate=rospy.Rate(10)
 	s=rospy.Service("/navigation/localization/prob_location", prob_localization, handle_prob_loc)
-
+	client_mvncdf=rospy.ServiceProxy("/navigation/localization/mvncdf",MVNCDF)
 	while not rospy.is_shutdown():
 
 		rate.sleep()
 
 if __name__ == '__main__':
-	file="/home/biorobotica/hugo_tesis/JUSTINA/catkin_ws/src/planning/knowledge/navigation/known_delimitation_hugo.txt"
 	try:
-	    if "-f" in sys.argv:
-        	file=sys.argv[sys.argv.index("-f") + 1]
-	    main(file)
+		file=""
+		if "-f" in sys.argv:
+			file=sys.argv[sys.argv.index("-f") + 1]
+		print (file)
+		main(file)
 	except rospy.ROSInterruptException:
-	    pass
+		pass
---
+++ catkin_ws/src/navigation/localization/src/mvncdf_server.cpp
@@ -0,0 +1,79 @@
+#include "ros/ros.h"
+#include "localization/MVNCDF.h"
+
+
+double mvn_cdf(double lx, double ly, double ux, double uy, double step, double mean_x, double mean_y, double (*cov)[2])
+{
+    double det = cov[0][0]*cov[1][1] - cov[1][0]*cov[0][1]; //determinante de la covarianza
+    //Lineas para calcular inversa de la covarianza
+    double icov00 =  cov[1][1];
+    double icov01 = -cov[0][1];
+    double icov10 = -cov[1][0];
+    double icov11 =  cov[0][0];
+    cov[0][0] = icov00/det;
+    cov[0][1] = icov01/det;
+    cov[1][0] = icov10/det;
+    cov[1][1] = icov11/det;
+    //denominador de la pdf
+    double den = sqrt(pow(2*M_PI, 2)*det);
+    double x, y;
+
+    double cdf = 0;
+    for(double x_ = lx; x_ <= ux; x_+=step)
+        for(double y_ = ly; y_ <= uy; y_+=step){
+            x=x_-mean_x;
+            y=y_-mean_y;
+            cdf+= step*step*exp(-0.5*(cov[0][0]*x*x + cov[0][1]*x*y + cov[1][0]*x*y + cov[1][1]*y*y))/den;
+        }
+
+    return cdf;
+}
+
+
+
+
+bool handle_mvncdf(localization::MVNCDF::Request &req,
+					localization::MVNCDF::Response &res){
+
+	double aux;
+	double cov[2][2]={{req.cov00,req.cov01},{req.cov10,req.cov11}};
+
+	//cov[0][0]=req.cov00;
+	//cov[0][1]=req.cov01;
+	//cov[1][0]=req.cov10;
+	//cov[1][1]=req.cov11;
+
+	/*std::cout<<"req.lx: "<<req.lx<<std::endl;
+    std::cout<<"req.ly: "<<req.ly<<std::endl;
+    std::cout<<"req.ux: "<<req.ux<<std::endl;
+    std::cout<<"req.uy: "<<req.uy<<std::endl;
+    std::cout<<"req.step: "<<req.step<<std::endl;
+    std::cout<<"req.mean_x: "<<req.mean_x<<std::endl;
+    std::cout<<"req.mean_y: "<<req.mean_y<<std::endl;
+    for(size_t i=0;i<2;i++){
+        for(size_t j=0;j<2;j++){
+            std::cout<<"cov["<<i<<"]["<<j<<"]: "<<cov[i][j]<<std::endl;
+        }
+    }*/
+
+	res.prob=mvn_cdf(req.lx, req.ly, req.ux, req.uy, req.step, req.mean_x, req.mean_y, cov);
+    std::cout<<"prob: "<<res.prob<<std::endl;
+	return true;
+
+
+}
+
+
+
+int main(int argc, char* argv[]){
+
+	std::cout<<"INITIALIZING MVNCDF SERVER"<<std::endl;
+	ros::init(argc, argv, "mvncdf_server");
+    ros::NodeHandle n;
+    
+    ros::ServiceServer service = n.advertiseService("/navigation/localization/mvncdf", handle_mvncdf);
+    ros::spin();
+
+
+	return 0;
+}
